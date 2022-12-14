ccf0d7f78b72374a025a41abc537665e9cd70843
LaughingLeader <laughingleader@gmail.com>
2022-12-13 23:05:16 -0600

    Update localization, add changelog

--- Mods/DebugTools_ce3b3bd2-eb86-46d4-8d4f-9a472105d18e/Story/RawFiles/Lua/Shared.lua
+++ Mods/DebugTools_ce3b3bd2-eb86-46d4-8d4f-9a472105d18e/Story/RawFiles/Lua/Shared.lua
@@ -1,1 +1,30 @@
+local SpeedTestData = {}
+local IgnoredState = {
+	Paused = true
+}
 
+local currentTimes = {}
+
+Ext.Events.GameStateChanged:Subscribe(function (e)
+	local from = tostring(e.FromState)
+	local to = tostring(e.ToState)
+	if from == "Menu" and to == "StartLoading" then
+		currentTimes[from] = nil
+	end
+	if from == "Running" then
+		currentTimes[from] = nil
+	end
+	if IgnoredState[from] or IgnoredState[to] then
+		return
+	end
+	currentTimes[to] = Ext.Utils.MonotonicTime()
+	if currentTimes[from] then
+		SpeedTestData[#SpeedTestData+1] = {
+			From = from,
+			To = to,
+			Time = Ext.Utils.MonotonicTime() - currentTimes[from]
+		}
+	end
+	currentTimes[from] = nil
+	Ext.IO.SaveFile(string.format("Dumps/StateSpeeds_%s.json", Ext.IsClient() and "Client" or "Server"), Ext.Json.Stringify(SpeedTestData))
+end)
